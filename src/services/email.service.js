a7768b00f93d6031b579c2517eb0c75efc15e16f
TaRCioS-CLaY <tarcios.clay@gmail.com>
2019-11-02 22:18:32 -0300

    Envio de email funcionando

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -1,24 +1,51 @@
 import conexao from './../database/conexao';
-
+import { enviarEmail } from './mailjet.api.service';
+// import sendemail from 'sendemail';
+// const Sendmail = sendemail;
 const banco = conexao();
 const modelos = banco.models;
 const Agendamento = modelos.agendamento;
+const Beneficiario = modelos.beneficiario;
+
 
 /**
  * Monitora o banco a procura de novos agendamentos
  */
-export const monitorarbanco = () =>{
-    setInterval(()=> {
-    getAllAgendamentos().then((dados)=> {
-        const agendamentos = verificaAgendamentos(dados);
-        if (!agendamentos.length) {
-           return console.log('sem agendamentos ', agendamentos)
-        }
-        console.log('Agendamentos ', agendamentos);
+export const monitorarbanco = () => {
+  setInterval(() => {
+    let beneficiarios;
+    getAllBeneficiarios().then((dados) => beneficiarios = dados);
+    
+    getAllAgendamentos().then((dados) => {
+      const agendamentos = verificaAgendamentos(dados);
+      if (!agendamentos.length) {
+        return console.log('sem agendamentos ', agendamentos)
+      }
+      agendamentos.forEach((agendamento) => {
+        const email = pegarEmail(beneficiarios, agendamento.nr_matricula)
+        // enviarEmail(
+        //  'email, 'Teste',
+        //  'Testando envio de email',
+        //   'Testando essa bagaça'
+        //   )
+        enviarEmail(
+        'email', 'Teste',
+        'Testando envio de email',
+         'Testando essa bagaça'
+         )
+          .then(() => alteraFlagNotificationNoBanco(agendamento));
+        alteraFlagNotificationNoBanco(agendamento);
+      })
+      console.log('Email enviado teoricamente')
+      // console.log('Agendamentos ', agendamentos);
     });
-    }
+  }
     , 10000);
 }
+/**
+ * Fim  do monitoramento
+ */
+
 
 
 /**
@@ -33,22 +60,37 @@ const getAllAgendamentos = async () => await Agendamento.findAll();
  * @returns {[]} Retorna um array com todos os agendamentos que estão com a flag de notificação como falsa no banco
  */
 const verificaAgendamentos = (agendamentos) =>
-    agendamentos.filter((agendamento) => (agendamento.vl_push_notificacao === 'f'));
-
-const enviarEmail = () => todo; //todo
+  agendamentos.filter((agendamento) => (agendamento.vl_push_notificacao === 'f'));
 
 /**
  * Recebe um agendamento e altera o valor da notificação no banco
  * @param {{}} agendamento Agendamento que vai sofrer o update
  * @returns {boolean} Retorna true caso dê tudo certo ou false caso dê errado
  */
-const alteraFlagNotificationNoBanco = (agendamento) => Agendamento.update({vl_push_notificacao: 't'},
-                                                                          {where: {ce_agenda: agendamento.ce_agenda}})
-                                                                          .then(() => {
-                                                                              console.log('Alterado com sucesso');
-                                                                              return true;
-                                                                          })
-                                                                          .catch((e) => {
-                                                                              console.error('Ocorreu um erro ao fazer update no banco. Erro: ', e);
-                                                                              return false;
-                                                                          });
+const alteraFlagNotificationNoBanco = (agendamento) =>
+  Agendamento.update({ vl_push_notificacao: 't' }, { where: { cd_agenda: agendamento.cd_agenda } })
+    .then(() => {
+      console.log('Alterado com sucesso');
+      return true;
+    })
+    .catch((e) => {
+      console.error('Ocorreu um erro ao fazer update no banco. Erro: ', e);
+      return false;
+    });
+
+/**
+ * Recupera todos os beneficiarios do banco
+ * @returns {Promise<[{}]>} Retorna um array com todos os beneficiarios
+ */
+const getAllBeneficiarios = async () => await Beneficiario.findAll();
+
+/**
+ * Pega o email da matricula passada
+ * @param {[]} beneficiarios Array de beneficiarios
+ * @param {string} matricula Matricula a ser encontrada no array
+ * @returns {string} O email correspondente a matricula informada
+ */
+const pegarEmail = (beneficiarios, matricula) => {
+  const beneficiario = beneficiarios.find((pessoa) => pessoa.nr_matricula == matricula)
+  return beneficiario.ds_email;
+};
---
+++ src/services/mailjet.api.service.js
@@ -0,0 +1,45 @@
+const mailjet = require('node-mailjet')
+    .connect('apiKey', 'apiSecret');
+    
+/**
+ * Envia um email utilizando a Api do Mailjet
+ * @param {string} destinatarioEmail Email do Destinatario
+ * @param {string} destinatarioNome Nome do Destinatario
+ * @param {string} assunto Assunto do email
+ * @param {string} corpo Corpo do email
+ * @param {string} [remetenteEmail='tarcios.clay@gmail.com'] Opcional
+ * @param {string} [remetenteNome='Clayton'] Opcional
+ * @returns {Promise} Um log do resultado
+ */
+const enviarEmail = (destinatarioEmail, destinatarioNome, assunto, corpo, remetenteEmail = 'email', remetenteNome = 'nome') => {
+    const request = mailjet
+        .post("send", { 'version': 'v3.1' })
+        .request({
+            "Messages": [
+                {
+                    "From": {
+                        "Email": remetenteEmail,
+                        "Name": remetenteNome
+                    },
+                    "To": [
+                        {
+                            "Email": destinatarioEmail,
+                            "Name": destinatarioNome
+                        }
+                    ],
+                    "Subject": assunto,
+                    "TextPart": "Meu primeiro email",
+                    "HTMLPart": `${corpo}`,
+                    "CustomID": "Projeto Integrador"
+                }
+            ]
+        });
+    return request
+        .then((result) => {
+            console.log(result.body)
+        })
+        .catch((err) => {
+            console.log(err.statusCode)
+        })
+
+}
